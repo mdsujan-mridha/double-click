541df3b7e4b59e82852b19116c6086c57ca88d12
mdsujon-mridha <mdsujan134523@gmail.com>
2022-02-12 21:13:13 +0600

    first commit

---
+++ home.js
@@ -0,0 +1,8 @@
+document.getElementById('box1').addEventListener('dblclick',function(){
+    document.getElementById('box1').style.backgroundColor='Teal';
+    
+})
+
+document.getElementById('box2').addEventListener('dblclick',function(){
+    document.getElementById('box2').style.backgroundColor='Fuchsia';
+}) 
